a7dbbeb0fabe41e623bf4a871ceb21dbe7d360c0
AritriBasu <AritriBasu@users.noreply.github.com>
2021-05-01 11:20:10 +0530

    fixed logi error

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
+import {Form, Button} from 'react-bootstrap';
 
 function LoginPage(){
 return (
-<MDBContainer>
-  <MDBRow>
-    <MDBCol md="6">
-      <form>
-        <p className="h5 text-center mb-4">Sign in</p>
-        <div className="grey-text">
-          <MDBInput label="Type your email" icon="envelope" group type="email" validate error="wrong"
-            success="right" />
-          <MDBInput label="Type your password" icon="lock" group type="password" validate />
-        </div>
-        <div className="text-center">
-          <MDBBtn>Login</MDBBtn>
-        </div>
-      </form>
-    </MDBCol>
-  </MDBRow>
-</MDBContainer>
+<div className="login" style={{width:"40%", textAlign:"left", marginRight:"40px"}}>
+<Form>
+  <Form.Group controlId="formBasic">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>80
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+</div>
 );
 }
 
