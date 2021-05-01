d2f65b5380bc88dfa46a172820f9384cb048d2a9
Sanskrita Saha <sanskrita2001@gmail.com>
2021-05-01 19:20:53 +0530

    Savings

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import LoginPage from '../src/components/Login';
-import SignupPage from '../src/components/Signup';
+// import LoginPage from '../src/components/Login';
+// import SignupPage from '../src/components/Signup';
 import NavbarComp from '../src/UI/Navbar';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
 import Helper from './UI/Helper';
 import Savings from './components/Savings/Savings';
 
@@ -19,9 +20,9 @@ function App() {
 						path='/signup'
 						component={SignupPage}
 					/> */}
+					<Route exact path='/savings' component={Savings}></Route>
 				</Switch>
 			</BrowserRouter>
-			<Savings/>
 		</>
 	);
 }
--- client/src/components/Savings/Savings.js
+++ client/src/components/Savings/Savings.js
@@ -1,39 +1,267 @@
-import React from 'react'
-import { Accordion,Card,Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import {
+	Accordion,
+	Card,
+	Button,
+	Form,
+	ListGroup,
+	Modal,
+} from 'react-bootstrap';
 const Savings = () => {
-    return (
-			<div className='savings'>
-				<div className='container'>
-					<div className='row'>
-						<div className='col-6'>
-							<h1 style={{ color: '#093321' }}>Start Saving</h1>
-							<Accordion>
-								<Card>
-									<Card.Header>
-										<Accordion.Toggle as={Button} variant='link' eventKey='0'>
-											Click me!
-										</Accordion.Toggle>
-									</Card.Header>
-									<Accordion.Collapse eventKey='0'>
-										<Card.Body>Hello! I'm the body</Card.Body>
-									</Accordion.Collapse>
-								</Card>
-								<Card>
-									<Card.Header>
-										<Accordion.Toggle as={Button} variant='link' eventKey='1'>
-											Click me!
-										</Accordion.Toggle>
-									</Card.Header>
-									<Accordion.Collapse eventKey='1'>
-										<Card.Body>Hello! I'm another body</Card.Body>
-									</Accordion.Collapse>
-								</Card>
-							</Accordion>
-						</div>
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+	return (
+		<div className='savings'>
+			<div className='container'>
+				<div className='row'>
+					<div className='col-12 col-md-6'>
+						<h1 style={{ marginTop: '30%', color: '#093321' }}>Start Saving</h1>
+						<Form.Group controlId='salary'>
+							<Form.Control type='text' placeholder='Gross Annual Salary' />
+							<Form.Text className='text-muted' style={{ color: '#2D8D64' }}>
+								We'll never share your details with anyone else.
+							</Form.Text>
+						</Form.Group>
+						<Accordion>
+							<Card style={{ backgroundColor: '#2D8D64' }}>
+								<Card.Header>
+									<Accordion.Toggle
+										className='text-light'
+										as={Button}
+										variant='link'
+										eventKey='0'
+									>
+										Additional Income
+									</Accordion.Toggle>
+								</Card.Header>
+								<Accordion.Collapse eventKey='0'>
+									<ListGroup variant='flush'>
+										<ListGroup.Item>
+											<Button
+												onClick={handleShow}
+												style={{ width: '100%', backgroundColor: '#4CBC8D' }}
+											>
+												Rental from Income
+											</Button>
+										</ListGroup.Item>
+										<ListGroup.Item>
+											<Button
+												onClick={handleShow}
+												style={{ width: '100%', backgroundColor: '#4CBC8D' }}
+											>
+												Interests from Deposits
+											</Button>
+										</ListGroup.Item>
+										<ListGroup.Item>
+											<Button
+												onClick={handleShow}
+												style={{ width: '100%', backgroundColor: '#4CBC8D' }}
+											>
+												Others
+											</Button>
+										</ListGroup.Item>
+									</ListGroup>
+								</Accordion.Collapse>
+								<Modal show={show} onHide={handleClose}>
+									<Modal.Header closeButton>
+										<Modal.Title>Rental from Income</Modal.Title>
+									</Modal.Header>
+									<Modal.Body>
+										<Form.Group>
+											<Form.Control type='text' />
+										</Form.Group>
+									</Modal.Body>
+									<Modal.Footer>
+										<Button variant='secondary' onClick={handleClose}>
+											Close
+										</Button>
+										<Button variant='primary' onClick={handleClose}>
+											Save Changes
+										</Button>
+									</Modal.Footer>
+								</Modal>
+							</Card>
+							<Card style={{ backgroundColor: '#2D8D64' }}>
+								<Card.Header>
+									<Accordion.Toggle
+										className='text-light'
+										as={Button}
+										variant='link'
+										eventKey='1'
+									>
+										Add Exemption
+									</Accordion.Toggle>
+								</Card.Header>
+								<Accordion.Collapse eventKey='1'>
+									<ListGroup variant='flush'>
+										<ListGroup.Item>
+											<Button
+												onClick={handleShow}
+												style={{ width: '100%', backgroundColor: '#4CBC8D' }}
+											>
+												House Rent Allowance
+											</Button>
+										</ListGroup.Item>
+										<ListGroup.Item>
+											<Button
+												onClick={handleShow}
+												style={{ width: '100%', backgroundColor: '#4CBC8D' }}
+											>
+												Leave Travel Allowance
+											</Button>
+										</ListGroup.Item>
+										<ListGroup.Item>
+											<Button
+												onClick={handleShow}
+												style={{ width: '100%', backgroundColor: '#4CBC8D' }}
+											>
+												Amount recieved from Gratuity
+											</Button>
+										</ListGroup.Item>
+										<ListGroup.Item>
+											<Button
+												onClick={handleShow}
+												style={{ width: '100%', backgroundColor: '#4CBC8D' }}
+											>
+												Company Leased Car
+											</Button>
+										</ListGroup.Item>
+										<ListGroup.Item>
+											<Button
+												onClick={handleShow}
+												style={{ width: '100%', backgroundColor: '#4CBC8D' }}
+											>
+												Coupons for Food
+											</Button>
+										</ListGroup.Item>
+										<ListGroup.Item>
+											<Button
+												onClick={handleShow}
+												style={{ width: '100%', backgroundColor: '#4CBC8D' }}
+											>
+												Expenses for Telephone and Internet
+											</Button>
+										</ListGroup.Item>
+									</ListGroup>
+								</Accordion.Collapse>
+								<Modal show={show} onHide={handleClose}>
+									<Modal.Header closeButton>
+										<Modal.Title>Rental from Income</Modal.Title>
+									</Modal.Header>
+									<Modal.Body>
+										<Form.Group>
+											<Form.Control type='text' />
+										</Form.Group>
+									</Modal.Body>
+									<Modal.Footer>
+										<Button variant='secondary' onClick={handleClose}>
+											Close
+										</Button>
+										<Button variant='primary' onClick={handleClose}>
+											Save Changes
+										</Button>
+									</Modal.Footer>
+								</Modal>
+							</Card>
+							<Card style={{ backgroundColor: '#2D8D64' }}>
+								<Card.Header>
+									<Accordion.Toggle
+										className='text-light'
+										as={Button}
+										variant='link'
+										eventKey='2'
+									>
+										Add Deduction
+									</Accordion.Toggle>
+								</Card.Header>
+								<Accordion.Collapse eventKey='2'>
+									<ListGroup variant='flush'>
+										<ListGroup.Item>
+											<Button
+												onClick={handleShow}
+												style={{ width: '100%', backgroundColor: '#4CBC8D' }}
+											>
+												PF Contribution
+											</Button>
+										</ListGroup.Item>
+										<ListGroup.Item>
+											<Button
+												onClick={handleShow}
+												style={{ width: '100%', backgroundColor: '#4CBC8D' }}
+											>
+												ELSS
+											</Button>
+										</ListGroup.Item>
+										<ListGroup.Item>
+											<Button
+												onClick={handleShow}
+												style={{ width: '100%', backgroundColor: '#4CBC8D' }}
+											>
+												Tuition fees of 2 children
+											</Button>
+										</ListGroup.Item>
+										<ListGroup.Item>
+											<Button
+												onClick={handleShow}
+												style={{ width: '100%', backgroundColor: '#4CBC8D' }}
+											>
+												Life Insurance Premium for self,spouse,and kids
+											</Button>
+										</ListGroup.Item>
+										<ListGroup.Item>
+											<Button
+												onClick={handleShow}
+												style={{ width: '100%', backgroundColor: '#4CBC8D' }}
+											>
+												Investing in National Savings Certificate Schemes
+											</Button>
+										</ListGroup.Item>
+										<ListGroup.Item>
+											<Button
+												onClick={handleShow}
+												style={{ width: '100%', backgroundColor: '#4CBC8D' }}
+											>
+												A 5-year term deposit
+											</Button>
+										</ListGroup.Item>
+									</ListGroup>
+								</Accordion.Collapse>
+								<Modal show={show} onHide={handleClose}>
+									<Modal.Header closeButton>
+										<Modal.Title>Rental from Income</Modal.Title>
+									</Modal.Header>
+									<Modal.Body>
+										<Form.Group>
+											<Form.Control type='text' />
+										</Form.Group>
+									</Modal.Body>
+									<Modal.Footer>
+										<Button variant='secondary' onClick={handleClose}>
+											Close
+										</Button>
+										<Button variant='primary' onClick={handleClose}>
+											Save Changes
+										</Button>
+									</Modal.Footer>
+								</Modal>
+							</Card>
+						</Accordion>
+						<Button variant='dark' size='lg' block style={{ margin: '10% 0' }}>
+							Calculate Tax
+						</Button>
+					</div>
+					<div
+						className='col-12 col-md-6'
+						style={{ backgroundColor: '#4CBC8D', marginTop:'10%' }}
+					>
+						<h3>Results</h3>
 					</div>
 				</div>
 			</div>
-		);
-}
+		</div>
+	);
+};
 
-export default Savings
+export default Savings;
